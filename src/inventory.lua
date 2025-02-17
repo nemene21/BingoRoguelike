d0f3cd8f7725576ae0f815b46ddf29c6f8eb04d1
Borna <borna.ozanic@gmail.com>
2025-02-17 15:48:02 +0100

    Slot graphic

--- src/inventory.lua
+++ src/inventory.lua
@@ -8,16 +8,6 @@ function Slot:new(x, y)
     self.tex_res = nil
     self.stack = nil
     self:add(TransComp(x, y))
-    self:add_drawable(SlotRenderer(self))
-end
-
-function SlotRenderer:new(slot)
-    Drawable.new(self)
-    self.slot = slot
-    self.layer = DrawLayers.UI
-end
-
-function SlotRenderer:_draw()
-    local slot = self.slot
-    lg.circle("fill", slot.Trans.pos.x, slot.Trans.pos.y, 8)
+    self:add_drawable("sprite", Sprite("assets/itemslot.png"))
+    self.sprite.layer = DrawLayers.UI
 end
