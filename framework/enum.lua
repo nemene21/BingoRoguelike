825b4661e0cc7c7f65a14ed9d53592ad489cce9c
Borna <borna.ozanic@gmail.com>
2025-02-17 18:37:09 +0100

    enum0

--- framework/enum.lua
+++ framework/enum.lua
@@ -10,3 +10,15 @@ function enum(tbl)
     })
     return enumerated
 end
+
+function enum0(tbl)
+    local enumerated = {}
+    for i, value in ipairs(tbl) do
+        enumerated[value] = i - 1
+    end
+    setmetatable(tbl, {
+        __index = function(tbl, key) error("ERROR: Constant '"..tostring(key).."' not in enum") end,
+        __newindex = function() error("ERROR: Enums are immutable") end
+    })
+    return enumerated
+end
