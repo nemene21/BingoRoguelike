6f76d6b7a89e6dd5c45cb6a5dc1df4f7a7a705c4
Borna <borna.ozanic@gmail.com>
2025-02-20 17:59:07 +0100

    Label

--- framework/label.lua
+++ framework/label.lua
@@ -1,6 +1,20 @@
 require "framework.drawable"
 
+font_manager = ResManager(function(path)
+    return lg.newFont(path, 8)
+end)
+DEFAULT_FONT = "assets/nokia.ttf"
+
 Label = class(Drawable)
-function Label:new(text, x, y)
+function Label:new(text, x, y, font)
+    Drawable.new(self)
     self.layer = DrawLayers.UI
+    self.text = text or ""
+    self.font_res = font_manager:get(font or DEFAULT_FONT)
+
+    self.pos:set(x or 0, y or 0)
+end
+
+function Label:_draw()
+    lg.print(self.text, self.font_res:get(), 0, 0)
 end
--- src/inventory.lua
+++ src/inventory.lua
@@ -1,5 +1,6 @@
 require "src.items"
 require "framework.ecs"
+require "framework.label"
 
 local SLOTSIZE = 12
 
@@ -9,6 +10,8 @@ function Slot:new(x, y)
     Entity.new(self)
     self.stack = nil
     self:add(TransComp(x, y))
+    
+    self:add_drawable("amount_label", Label())
 
     self:add_drawable("item_sprite", Spritesheet("assets/itemsheet.png", 8, 8))
     self.item_sprite.pos:setv(self.Trans.pos)
@@ -19,6 +22,7 @@ function Slot:new(x, y)
     self:add_drawable("sprite", Sprite("assets/itemslot.png"))
     self.sprite.layer = DrawLayers.UI
     self.sprite.pos:setv(self.Trans.pos)
+
 end
 
 function Slot:set_mouse_slot(slot)
@@ -35,6 +39,10 @@ function Slot:update_data()
     if self.stack.amount == 0 then self:clear() return nil end
     self.item_sprite.framepos.x = self.stack.data.tex_id
     self.item_sprite:show()
+
+    self.amount_label.text = tostring(self.stack.amount)
+    self.amount_label.pos:setv(self.sprite.pos)
+    self.amount_label.pos:add(2)
 end
 
 function Slot:set_stack(stack)
@@ -64,7 +72,6 @@ function Slot:_clicked()
     elseif item_compare(mouse_stack.data, self.stack.data) then
         self.stack:take_all_from(mouse_stack)
         self.mouse_slot:update_data()
-        print(self.mouse_slot.amount)
     else
         self.mouse_slot:set_stack(self.stack)
         self:set_stack(mouse_stack)
@@ -75,9 +82,12 @@ MouseSlot = class(Slot)
 function MouseSlot:new()
     Slot.new(self, 0, 0)
     self.sprite:hide()
+    self:_process(0)
 end
 
 function MouseSlot:_process(delta)
     self.Trans.pos:set(mouse_pos())
     self.item_sprite.pos:setv(self.Trans.pos)
+    self.amount_label.pos:setv(self.item_sprite.pos)
+    self.amount_label.pos:add(2)
 end
